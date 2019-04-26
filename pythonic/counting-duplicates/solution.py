1f8ffaed4cc5b0b71dac7c6700092b35ebff99f9
wtnerb <wtnerb@gmail.com>
2019-04-26 13:16:34 -0700

    duplicate count works for lower case strings

--- pythonic/counting-duplicates/solution.py
+++ pythonic/counting-duplicates/solution.py
@@ -1,9 +1,11 @@
 
 def duplicate_count(s):
-    seen = set()
+    seen = {}
     count = 0
     for c in s:
-        if c in seen:
+        if c in seen and seen[c]:
             count+=1
-        seen.add(c)
+            seen[c] = False
+        if not c in seen:
+            seen[c] = True
     return count
--- pythonic/counting-duplicates/solution_test.py
+++ pythonic/counting-duplicates/solution_test.py
@@ -3,4 +3,4 @@ from solution import duplicate_count as dc
 def test_lower_letters () :
     assert dc('abcc') == 1
     assert dc('abc') == 0
-    # assert dc('abbcccccc') == 2
+    assert dc('abbcccccc') == 2
