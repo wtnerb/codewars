12b9ba292cb8a83c6f1f2a161a0c9564f4cec8ee
wtnerb <wtnerb@gmail.com>
2019-04-26 11:43:02 -0700

    finished and polished square digits

--- pythonic/square-digits/solution_test.py
+++ pythonic/square-digits/solution_test.py
@@ -4,3 +4,8 @@ def test_single_digit():
     assert solution.square_digits(3) == 9
     assert solution.square_digits(2) == 4
     assert solution.square_digits(4) == 16
+
+def test_many_digits():
+    tests = ((11, 11), (12, 14), (15, 125))
+    for test in tests:
+        assert solution.square_digits(test[0]) == test[1]
