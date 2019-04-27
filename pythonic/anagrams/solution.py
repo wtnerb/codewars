97c4d3b0569dcff23d5ce6bf27d851bb6768b06f
wtnerb <wtnerb@gmail.com>
2019-04-26 17:21:28 -0700

    solved the anagram problem -- other solutions were much simpler

--- pythonic/anagrams/solution.py
+++ pythonic/anagrams/solution.py
@@ -1,4 +1,25 @@
 
 def anagrams(word, words):
-    #your code here
-    pass
+    words = (w.lower() for w in words if len(w) == len(word))
+    # build dictionary
+    letters = {}
+    for char in word:
+        if char in letters:
+            letters[char] += 1
+        else:
+            letters[char] = 1
+
+    l = letters.items()
+    anagramsList = []
+
+    for w in words:
+        letters = {k:v for k,v in l}
+        for char in w:
+            if not char in letters:
+                break
+            if letters[char] <= 0:
+                break
+            letters[char]-= 1
+        else:
+            anagramsList.append(w)
+    return anagramsList
--- pythonic/anagrams/solution_test.py
+++ pythonic/anagrams/solution_test.py
@@ -2,4 +2,4 @@ from solution import anagrams as a
 
 def test_simple ():
     assert a('abba', ['aabb', 'abcd', 'bbaa', 'dada']) == ['aabb', 'bbaa']
-    
+    assert a('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) == ['carer', 'racer']
